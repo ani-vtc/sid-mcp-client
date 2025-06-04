da8a30212b080ccfdf89b7200967e94907cb9863
StrawberryMilsc <51016465+StrawberryMilsc@users.noreply.github.com>
2025-06-04 15:32:34 -0700

    changed method for get databases

--- server.js
+++ server.js
@@ -399,7 +399,11 @@ app.get('/api/databases', async (req, res) => {
       [rows] = await connection.execute('SHOW DATABASES');
       await connection.end();
     } else {
-      rows = await getDatabasesProd();
+      rows = await anyQuery({
+        prj: process.env.PRJ,
+        select: "*",
+        conditions: ["SHOW DATABASES"]
+      });
       console.log('rows:', rows);
     }
     if (!rows || rows.length === 0) {
